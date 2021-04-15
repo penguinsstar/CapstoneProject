022e9436c55f5e91267aff72fc2ea711dfaa1f32
Khalil-Ammar <31873335+Khalil-Ammar@users.noreply.github.com>
2021-04-15 13:33:05 -0700

    Change arduino script name and update matlab script

--- Matlab/PPG.m
+++ Matlab/PPG.m
@@ -1,21 +1,25 @@
-
 %% Variables
 lowPassFc = 20;
 highPassFc = 0.3;
 SamplingFrequency = 200;
 butterworthOrder = 2;
 smoothingWindowSize = 20;
+baudRate = 115200;
 
 %% Configure Arduino Serial Port
-% arduinoObj = serialport('COM7',9600);
+% arduinoObj = serialport('COM7',baudRate);
 configureTerminator(arduinoObj,"CR/LF");
 flush(arduinoObj);
 arduinoObj.UserData = struct("Data",[],"Count",1);
 configureCallback(arduinoObj,"terminator",@readSensorData);
-% wait until data is collected
-pause(5);
 
-%% Apply high pass filter 
+% wait until data is collected
+while(~(arduinoObj.BytesAvailableFcnMode == "off"))
+    pause(1); %% wait for a second
+end
+
+%% Apply high pass filter
+arduinoObj.UserData.Data = arduinoObj.UserData.Data(3:end);
 HpFilteredData = highpass(arduinoObj.UserData.Data, highPassFc, SamplingFrequency);
 figure(2);
 title('After HPF');
@@ -29,11 +33,16 @@ title('After LPF');
 plot(LpFilteredData);
 
 %% Moving Average
-movingAverageWindowSize = 10*SamplingFrequency; %% about 10 cardiac cycles assuming 1 beat/sec
-averagedFileteredData = movmean(LpFilteredData, movingAverageWindowSize);
+movingAverageWindowSize = 40;
+averagedData = movmean(LpFilteredData, movingAverageWindowSize);
+smoothedData = smoothdata(LpFilteredData);
 figure(4);
-title('After moving average');
-plot(averagedFilteredData);
+title('Heuristically Smoothed Data');
+plot(smoothedData);
+
+figure(5);
+title('Moving Average (Window = 2 beats)');
+plot(averagedData);
 
 
 
--- Matlab/readSensorData.m
+++ Matlab/readSensorData.m
@@ -2,19 +2,31 @@ function readSensorData(src, ~)
 
     % Read the ASCII data from the serialport object.
     data = readline(src);
+    data = split(data);
 
     % Convert the string data to numeric type and save it in the UserData
     % property of the serialport object.
-    src.UserData.Data(end+1) = str2double(data);
+    src.UserData.Data.PPG(end+1) = str2double(data(3));
+    src.UserData.Data.ECG(end+1) = str2double(data(1));
+    src.UserData.Data.SCG(end+1) = str2double(data(2));
 
     % Update the Count value of the serialport object.
-    src.UserData.Count = src.UserData.Count + 1;
+    src.UserData.Data.Count = src.UserData.Data.Count + 1;
 
     % If 1001 data points have been collected from the Arduino, switch off the
     % callbacks and plot the data.
-    if src.UserData.Count > 200*15
+    if src.UserData.Data.Count > 500
         configureCallback(src, "off");
         figure(1);
-        plot(src.UserData.Data(2:end));
+        src.UserData.Data.NormPPG = normalize(src.UserData.Data.PPG(2:end));
+        src.UserData.Data.NormECG = normalize(src.UserData.Data.ECG(2:end));
+        src.UserData.Data.NormSCG = normalize(src.UserData.Data.SCG(2:end));
+        
+        hold on;
+        plot(src.UserData.Data.NormPPG);
+        plot(src.UserData.Data.NormECG);  
+        plot(src.UserData.Data.NormSCG);
+        legend('PPG', 'ECG', 'SCG');
+        hold off;
     end
 end
